a28e5fee26ed27a70146e1a2a0fdeea56077cd5d
Peter Krevenets <peter.k.nets@gmail.com>
2017-07-26 11:35:54 +0300

    Add and pass fetch action tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,24 @@
 import * as types from '../constants/ActionTypes';
 
 export const fetch30Request = () => ({
-
+  type: types.FETCH_30_REQUEST,
 });
 export const fetch30Success = (body) => ({
-
+  type: types.FETCH_30_SUCCESS,
+  body
 });
 export const fetch30Failure = (ex) => ({
-
+  type: types.FETCH_30_FAILURE,
+  ex
 });
 export const fetchAllRequest = () => ({
-
+  type: types.FETCH_ALL_REQUEST,
 });
 export const fetchAllSuccess = (body) => ({
-
+  type: types.FETCH_ALL_SUCCESS,
+  body
 });
 export const fetchAllFailure = (ex) => ({
-
+  type: types.FETCH_ALL_FAILURE,
+  ex
 });
--- src/tests/actions/index.test.js
+++ src/tests/actions/index.test.js
@@ -1,10 +1,50 @@
 import * as types from '../../constants/ActionTypes';
 import * as actions from '../../actions';
 
-describe('actions', () => {
+describe('fetch30 actions', () => {
   it('fetch30Request should create FETCH_30_REQUEST action', () => {
     expect(actions.fetch30Request()).toEqual({
       type: types.FETCH_30_REQUEST,
+    });
+  });
+  it('fetch30Success should create FETCH_30_SUCCESS action', () => {
+    const body = {name: 'Bobby', score: 300};
+
+    expect(actions.fetch30Success(body)).toEqual({
+      type: types.FETCH_30_SUCCESS,
+      body
+    });
+  });
+  it('fetch30Failure should create FETCH_30_FAILURE action', () => {
+    const ex = {type: 'exception'};
+
+    expect(actions.fetch30Failure(ex)).toEqual({
+      type: types.FETCH_30_FAILURE,
+      ex
+    });
+  });
+});
+
+describe('fetchAll actions', () => {
+  it('fetchAllRequest should create FETCH_ALL_REQUEST action', () => {
+    expect(actions.fetchAllRequest()).toEqual({
+      type: types.FETCH_ALL_REQUEST,
+    });
+  });
+  it('fetchAllSuccess should create FETCH_ALL_SUCCESS action', () => {
+    const body = {name: 'Bobby', score: 300};
+
+    expect(actions.fetchAllSuccess(body)).toEqual({
+      type: types.FETCH_ALL_SUCCESS,
+      body
+    });
+  });
+  it('fetchAllFailure should create FETCH_ALL_FAILURE action', () => {
+    const ex = {type: 'exception'};
+
+    expect(actions.fetchAllFailure(ex)).toEqual({
+      type: types.FETCH_ALL_FAILURE,
+      ex
     });
   });
 });
