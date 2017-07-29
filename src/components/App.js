69baf30a984142bc8ea61705933b525b18db3822
Peter Krevenets <peter.k.nets@gmail.com>
2017-07-29 19:53:11 +0300

    Attempt to create a table using flexbox

--- src/components/App.js
+++ src/components/App.js
@@ -25,13 +25,30 @@ export class App extends Component {
               <img src={user.img} />
               <span>{user.username}</span>
             </a>
-            {/* {user.username} */}
           </td>
           <td>{user.recent}</td>
           <td>{user.alltime}</td>
         </tr>
       );
     });
+
+    // const flexTable = score30days.map((user, index) => {
+    //   return (
+    //     <div key={user.username} className="table-row">
+    //       <div className="username-number">{index + 1}</div>
+    //       <div className="username-column">
+    //         <a
+    //           target="blank"
+    //           href={`https://www.freecodecamp.com/${user.username}`}>
+    //           <img src={user.img} />
+    //           <span>{user.username}</span>
+    //         </a>
+    //       </div>
+    //       <div>{user.recent}</div>
+    //       <div>{user.alltime}</div>
+    //     </div>
+    //   );
+    // });
 
     return (
       <div className="app">
@@ -46,8 +63,8 @@ export class App extends Component {
               <tr>
                 <th>#</th>
                 <th>Camper</th>
-                <th>Top 30 Days</th>
-                <th>Top All Time</th>
+                <th>30 Days</th>
+                <th>All Time</th>
               </tr>
             </thead>
             <tbody>
@@ -55,6 +72,18 @@ export class App extends Component {
             </tbody>
           </table>
         </section>
+
+        {/* <section className="table">
+          <div className="table-header">
+            <div>#</div>
+            <div>Camper</div>
+            <div>30 Days</div>
+            <div>All Time</div>
+          </div>
+          <div className="table-body">
+            {flexTable}
+          </div>
+        </section> */}
 
         <footer>
           <p>2017, created by <a target="blank" href="https://github.com/GuRuGuMaWaRu">Peter Krevenets</a></p>
